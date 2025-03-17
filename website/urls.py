10dd57dfebbe98200f24ef40b2c72d18d50feaea
Gerbasio34 <torresbarrancogerman3@gmail.com>
2025-03-17 11:13:26 +0100

    ADD: login, forms and other things

--- website/urls.py
+++ website/urls.py
@@ -5,5 +5,9 @@ from . import views
 # Routes that can we access in our websites
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    # ----------- Login --------------
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ] 
---
+++ website/views/autentificator.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+#from .forms import CustomUserCreationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegister(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserRegister()
+    return render(request, 'register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+    return render(request, 'login.html') #redirigir a otra pagina, home con cookies
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')
