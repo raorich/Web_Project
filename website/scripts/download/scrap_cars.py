5ff1121c359a2e277d4c10e957b967e11792eba1
Rapite46 <raulsotoimedio@outlook.es>
2025-05-05 09:58:14 +0200

    ADD: Scrap Cars and Art

--- website/scripts/download/scrap_cars.py
+++ website/scripts/download/scrap_cars.py
@@ -3,11 +3,12 @@ import sys
 
 from pprint import pprint
 from urllib.parse import urljoin
-from bs4 import BeautifulSoup
 
-from scrapping_utils import WebScrapper
+from scrapping_utils import WebScrapper, GenerateCSV
 
 BASE_URL: str = 'https://classiccars.com/'
+
+FEATURED_LIST = 'classic-cars/featured-listings?ps=60'
 
 cookies = {
     '_cctkn_': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1bmlxdWVfbmFtZSI6IkFub255bW91cyBVc2VyIiwianRpIjoiZDVjZDRkYTAtNmEwOC00ZTU2LTg3OTEtNzljMTFhMzhjMjMwIiwiaWF0IjoiMTc0NTI1OTI4NC45MzY5NCIsImlzcyI6IkNsYXNzaWNDYXJzLmNvbSIsImF1ZCI6IkFueSIsImV4cCI6MTc0NTI1OTI4NCwibmJmIjoxNzQ0MDQ5Njg0fQ.8o9lJy7wNF411YnuwNc5TczrIobmd0MqhIo9HZYy7Vk',
@@ -30,7 +31,143 @@ headers = {
     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
 }
 
-series_soup = WebScrapper(BASE_URL, headers=headers, cookies=cookies).get_soup()
+#GET FEATURED CARS URL
+href_list = set()
+for page in range(1,5):
+    url_plus_page = f"{FEATURED_LIST}&p={page}" if page > 1 else FEATURED_LIST
+    series_soup = WebScrapper(urljoin(BASE_URL,url_plus_page), headers=headers, cookies=cookies).get_soup()
 
-slick_list = series_soup.find("div", id="FSBOslider").find_all("div", class_="fsbo-slider")
-pprint(series_soup)
+    #pprint(series_soup)
+    cars_div = series_soup.find_all("div", class_="search-result-item w100 featured")
+    for car in cars_div:
+        href_list.add(car.find("a").get('href',''))
+
+'''
+name -> header
+description -> Exterior Color + transmission + (algo del engine)
+category -> "car"
+starting_price -> price (lleva les ',' llevar '(OBO)', llevar $, "Contact Seller" = 400.000 600.000
+reserve_price  -> price
+auction_end_time -> x
+store -> x
+images -> pillar las images
+
+brand -> "Make"
+restoration -> Restoration History: 
+transmission -> transmission
+model -> Model
+Engine condition -> "Engine History" + "Engine Condition" 
+year -> Year
+
+href_list.add('/listings/view/1894134/1948-plymouth-2-dr-sedan-for-sale-in-vonore-tennessee-37885')
+'''
+
+final_data = []
+for href in href_list:
+    car_soup = WebScrapper(urljoin(BASE_URL,href), headers=headers, cookies=cookies).get_soup()
+
+    ####### IMGS #######
+
+    image_div = car_soup.find("div",class_="swiper-wrapper").find_all("img", class_=["u-photo", "img-fluid"])
+    images_url = [WebScrapper.normalizeGetText(ima.get('data-src','')) for ima in image_div]
+    images_url = [x for x in images_url if x.strip()]
+    
+    ##################################
+
+    ####### MORE DETAILS #######
+    
+    car_detail = car_soup.find("div", class_="vehicle-details").find("ul")
+    
+    header =  WebScrapper.normalizeGetText(car_detail.find("li", class_=["p-name"]).find("span").getText())
+
+    # PRECIO
+    data_soup = car_detail.find("li", class_=["p-price"])
+    if data_soup:
+        list_span = data_soup.find_all("span")
+        price = ""
+        if len(list_span) >= 2:
+            price  =  WebScrapper.normalizeGetText(list_span[1].getText())
+
+    # EXTERIOR COLOR
+    data_soup = car_detail.find("li", class_=["p-color"])
+    exterior_color = ""
+    if data_soup:
+        list_span = data_soup.find_all("span")
+        if len(list_span) >= 2:
+            exterior_color  =  WebScrapper.normalizeGetText(list_span[1].getText())
+
+    # TRANSMISSION
+    data_soup = car_detail.find("li", class_=["p-transmission"])
+    transmission = ""
+    if data_soup:
+        list_span = data_soup.find_all("span")
+        if len(list_span) >= 2:
+            transmission  =  WebScrapper.normalizeGetText(list_span[1].getText())
+
+    # MODEL
+    data_soup = car_detail.find("li", class_=["p-model"])
+    model = ""
+    if data_soup:
+        list_span = data_soup.find_all("span")
+        if len(list_span) >= 2:
+            model  =  WebScrapper.normalizeGetText(list_span[1].getText())
+
+    # BRAND
+    data_soup = car_detail.find("li", class_=["p-manufacturer"])
+    brand = ""
+    if data_soup:
+        list_span = data_soup.find_all("span")
+        if len(list_span) >= 2:
+            brand  =  WebScrapper.normalizeGetText(list_span[1].getText())
+
+    # YEAR
+    data_soup = car_detail.find("li", class_=["dt-start"])
+    year = ""
+    if data_soup:
+        list_span = data_soup.find_all("span")
+        if len(list_span) >= 2:
+            year  =  WebScrapper.normalizeGetText(list_span[1].getText())
+
+    # ENGINE CONDITION
+    data_soup = car_detail.find("li", class_=["p-condition"])
+    engine_condition = ""
+    if data_soup:
+        list_span = data_soup.find_all("span")
+        if len(list_span) >= 2:
+            engine_condition  =  WebScrapper.normalizeGetText(list_span[1].getText())
+
+    # RESTORATION
+    restoration = ""
+    for featured in car_detail.find_all("li", class_=["p-feature"]):
+        list_span = featured.find_all("span")
+        if not len(list_span) >= 2: continue
+        if "restoration" in list_span[0].getText().lower():
+            restoration  =  WebScrapper.normalizeGetText(list_span[1].getText())
+            break
+    
+    # ENGINE HISTORY
+    engine_history = ""
+    for featured in car_detail.find_all("li", class_=["border-btm"]):
+        list_span = featured.find_all("span")
+        if not len(list_span) >= 2: continue
+        if "engine history" in list_span[0].getText().lower():
+            engine_history  =  WebScrapper.normalizeGetText(list_span[1].getText())
+            break
+
+    ##################################
+
+    row = {}
+    row["Header"] = header
+    row["Price"] =  price
+    row["Exterior Color"] =  exterior_color
+    row["Transmission"] =  transmission
+    row["Restoration"] =  restoration
+    row["Model"] =  model
+    row["Brand"] =  brand
+    row["Engine Condition"] =  f"{engine_history} - {engine_condition}"
+    row["Year"] =  year
+    row["Imgs"] =  images_url
+    final_data.append(row)
+    
+GenerateCSV("Cars_data_to_upload", final_data).generate()
+sys.exit()
--- website/scripts/download/scrapping_utils.py
+++ website/scripts/download/scrapping_utils.py
@@ -32,12 +32,14 @@ class WebScrapper():
                 self.response = response
                 soup: BeautifulSoup = BeautifulSoup(response.text, 'html.parser')
                 self.soup = soup
+                #if "JavaScript is disabled" in self.soup.find_all("h1"): raise requests.exceptions.RequestException("Javascript error")
+                t = self.retry
                 break
             except requests.exceptions.RequestException as e:
                 print(f"Error al realizar la solicitud: {e}")
                 t -= 1
                 if t <= 0: break
-                time.sleep(5)
+                time.sleep(10)
                 
     def get_soup(self) -> BeautifulSoup:
         return self.soup
@@ -56,7 +58,7 @@ class WebScrapperSelenium():
         "class": By.CLASS_NAME
     }
     
-    def __init__(self, url: str, element_name = "", locator = "", timeout = 10, headless = False):
+    def __init__(self, url: str, element_name = "", locator = "", timeout = 10, headless = False, base_path = None, cookies = []):
         self.url: str = url
         self.soup = None 
         options = Options()
@@ -67,15 +69,31 @@ class WebScrapperSelenium():
             options.add_argument('window-size=1200x600')  
         options.add_argument('--no-sandbox')
         self.driver = webdriver.Chrome(options=options)
+
+        self.driver.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument', {
+            'source': '''
+                Object.defineProperty(navigator, 'webdriver', {
+                    get: () => undefined
+                })
+            '''
+        })
+
         self.timeout = timeout
         self.element_name = element_name
         self.locator = locator
+        self.base_path = base_path
+        self.cookies = cookies
         self.read_url()
 
     def read_url(self):
         while True:
             print(f'Requesting url: {self.url}')
             try:
+                if self.base_path and self.cookies:
+                    self.driver.get(self.base_path)
+                    for cookie in self.cookies:
+                        self.driver.add_cookie(cookie)
+
                 self.driver.get(self.url)
                 locator_value = WebScrapperSelenium.LOCATOR_MAP.get(self.locator,None)
 
@@ -86,8 +104,9 @@ class WebScrapperSelenium():
                         )
                     except selTimeout as e:
                         print("Timeout esperando el contenido")
-                        self.driver.quit()
-                        break
+                        #self.driver.quit()
+                        time.sleep(10)
+                        continue
                 else:
                     time.sleep(5)
 
