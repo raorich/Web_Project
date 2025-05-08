4a9950b7be56cc79db34f75ffc839a9c9f9bea99
Gerbasio34 <torresbarrancogerman3@gmail.com>
2025-05-08 17:48:15 +0200

    FIX: some stashed changes

--- website/views/pages.py
+++ website/views/pages.py
@@ -1,22 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import logout as auth_logout
 from django.shortcuts import redirect
-<<<<<<< Updated upstream
-from ..models import Product
-from .products_functions import get_random_features
 from django.http import HttpResponse, JsonResponse
-=======
 
+from ..models import Product
 from .products_functions import get_random_features, get_quote
->>>>>>> Stashed changes
+
+
 
 
 def home(request):
     products = get_random_features(20)
-<<<<<<< Updated upstream
-    from pprint import pprint
-    pprint([product.name for product in products])
-    return render(request, 'home.html', {"products": products})
+    quote = get_quote()
+    return render(request, 'home.html', {"products": products, "quote": quote})
 
 def product_detail(request, product_id):
     product = get_object_or_404(Product, id=product_id)
@@ -41,17 +37,16 @@ def product_detail(request, product_id):
     })
 
 def search_products(request):
+
     q = request.GET.get('q', '')
     products = Product.objects.filter(name__icontains=q)[:12]
     html = render(request, 'components/product_cards_ajax.html', {'products': products}).content.decode('utf-8')
     return JsonResponse({'html': html})
 
 def pujar_producto(request, product_id):
+
     return HttpResponse(f"Has llegado a la página de puja del producto con ID {product_id}")
-=======
-    quote = get_quote()
-    return render(request, 'home.html', {"products": products, "quote": quote})
+    
     
 
 
->>>>>>> Stashed changes
